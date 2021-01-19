2bfb0b92f56375371a9398e9377815a245bb9e86
phtevenIsChonky <53917447+phtevenIsChonky@users.noreply.github.com>
2021-01-19 17:49:46 -0600

    Updated from if/elseif/else to Switch
    Updated the script to use Switch instead of the string of if/elseif/else statements I had.

--- DownloadedFile_HashCheck.ps1
+++ DownloadedFile_HashCheck.ps1
@@ -6,20 +6,13 @@ $DownloadedFile = (Get-ChildItem $env:userprofile\Downloads\ | Sort-Object LastW
 
 $Checksum = Read-Host -Prompt "Please enter the known-good checksum from the download source"
 
-    if ($Checksum.length -eq 32){
-        $Hash = "MD5"
-    }
-    elseif ($Checksum.length -eq 40) {
-        $Hash = "SHA1"
-    }
-    elseif ($Checksum.length -eq 64) {
-        $Hash = "SHA256"
-    }
-    else {
-        Throw Read-Host "
-    The only supported hash algorithms at this time are SHA256, SHA1, and MD5.
-        
-    Press Enter to exit"
+Switch ($Checksum.Length){
+    {$_ -eq 32}{$Hash = "MD5"}
+    {$_ -eq 40}{$Hash = "SHA1"}
+    {$_ -eq 64}{$Hash = "SHA256"}
+    Default {throw Read-Host "
+    Hash length does not match SHA256, SHA1 or MD5.  
+    Press Enter to exit"}
     }
 
 $DownloadedFileHash = (Get-FileHash -Path $DownloadedFile -Algorithm $Hash).Hash 
